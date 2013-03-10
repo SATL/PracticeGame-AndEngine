6133391518e8f1ecf740ccee2e308416eaa96b83
Eslem <j.eslem03@gmail.com>
2013-03-10 19:37:03 +0100

    first commit

---
+++ src/com/example/testgame/MainActivity.java
@@ -0,0 +1,272 @@
+package com.example.testgame;
+
+import org.andengine.engine.camera.BoundCamera;
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl;
+import org.andengine.engine.camera.hud.controls.AnalogOnScreenControl.IAnalogOnScreenControlListener;
+import org.andengine.engine.camera.hud.controls.BaseOnScreenControl;
+import org.andengine.engine.camera.hud.controls.BaseOnScreenControl.IOnScreenControlListener;
+import org.andengine.engine.camera.hud.controls.DigitalOnScreenControl;
+import org.andengine.engine.handler.IUpdateHandler;
+import org.andengine.engine.handler.physics.PhysicsHandler;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.primitive.Rectangle;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.ButtonSprite;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.util.FPSLogger;
+import org.andengine.extension.physics.box2d.FixedStepPhysicsWorld;
+import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
+import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.extension.tmx.TMXLayer;
+import org.andengine.extension.tmx.TMXLoader;
+import org.andengine.extension.tmx.TMXProperties;
+import org.andengine.extension.tmx.TMXTile;
+import org.andengine.extension.tmx.TMXTileProperty;
+import org.andengine.extension.tmx.TMXTiledMap;
+import org.andengine.extension.tmx.TMXLoader.ITMXTilePropertiesListener;
+import org.andengine.extension.tmx.util.exception.TMXLoadException;
+import org.andengine.input.touch.TouchEvent;
+import org.andengine.input.touch.controller.MultiTouch;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.Constants;
+import org.andengine.util.debug.Debug;
+import org.andengine.util.math.MathUtils;
+
+import android.hardware.SensorManager;
+import android.opengl.GLES20;
+import android.widget.Toast;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+
+public class MainActivity extends SimpleBaseGameActivity{
+
+	private static final int CAMERA_WIDTH = 800;
+	private static final int CAMERA_HEIGHT = 480;
+
+	private BitmapTextureAtlas mBitmapTextureAtlas;
+	private TiledTextureRegion mPlayerTextureRegion;
+	
+	private Camera mCamera;
+	
+	private BitmapTextureAtlas mOnScreenControlTexture;
+	private ITextureRegion mOnScreenControlBaseTextureRegion;
+	private ITextureRegion mOnScreenControlKnobTextureRegion;
+	private DigitalOnScreenControl mDigitalOnScreenControl;
+	
+
+	private PhysicsWorld mPhysicsWorld;
+	
+	
+
+	Scene mScene = new Scene();
+	
+	private boolean direct;
+	private boolean running=false;
+	
+	private Body playerbody;
+	
+	private BitmapTextureAtlas mButtonTexture;
+	private ITextureRegion mButtonTextureRegion;
+	private boolean mPlaceOnScreenControlsAtDifferentVerticalLocations = false;
+	
+	
+	private BoundCamera mBoundChaseCamera;
+
+	
+	private TMXTiledMap mTMXTiledMap;
+	
+	
+	
+	@Override
+	public EngineOptions onCreateEngineOptions() {
+		// TODO Auto-generated method stub
+		mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		
+		final EngineOptions engineOptions = new EngineOptions(true, ScreenOrientation.LANDSCAPE_FIXED, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera);
+		engineOptions.getTouchOptions().setNeedsMultiTouch(true);
+
+		if(MultiTouch.isSupported(this)) {
+			if(MultiTouch.isSupportedDistinct(this)) {
+				Toast.makeText(this, "MultiTouch detected --> Both controls will work properly!", Toast.LENGTH_SHORT).show();
+			} else {
+				this.mPlaceOnScreenControlsAtDifferentVerticalLocations = true;
+				Toast.makeText(this, "MultiTouch detected, but your device has problems distinguishing between fingers.\n\nControls are placed at different vertical locations.", Toast.LENGTH_LONG).show();
+			}
+		} else {
+			Toast.makeText(this, "Sorry your device does NOT support MultiTouch!\n\n(Falling back to SingleTouch.)\n\nControls are placed at different vertical locations.", Toast.LENGTH_LONG).show();
+		}
+
+		return engineOptions;	}
+
+	@Override
+	protected void onCreateResources() {
+		// TODO Auto-generated method stub
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("files/");
+		
+		this.mBitmapTextureAtlas = new BitmapTextureAtlas(this.getTextureManager(), 300, 128, TextureOptions.BILINEAR);
+		this.mPlayerTextureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(this.mBitmapTextureAtlas, this, "player.png", 0, 0, 3, 4);
+		this.mBitmapTextureAtlas.load();
+		
+		this.mOnScreenControlTexture = new BitmapTextureAtlas(this.getTextureManager(), 256, 128, TextureOptions.BILINEAR);
+		this.mOnScreenControlBaseTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mOnScreenControlTexture, this, "onscreen_control_base.png", 0, 0);
+		this.mOnScreenControlKnobTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mOnScreenControlTexture, this, "onscreen_control_knob.png", 128, 0);
+		this.mOnScreenControlTexture.load();
+		
+		this.mButtonTexture=new BitmapTextureAtlas(this.getTextureManager(), 256, 128, TextureOptions.BILINEAR);
+		this.mButtonTextureRegion=BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.mButtonTexture, this, "onscreen_control_knob.png", 0, 0);
+		this.mButtonTexture.load();		
+		
+	}
+
+	@Override
+	protected Scene onCreateScene() {
+		// TODO Auto-generated method stub
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+		
+		final VertexBufferObjectManager vertexBufferObjectManager = this.getVertexBufferObjectManager();
+		
+		mScene.setBackground(new Background(0.09804f, 0.6274f, 0.8784f));
+		
+		
+		/* Calculate the coordinates for the face, so its centered on the camera. */
+		final float centerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getWidth()) / 2;
+		final float centerY = (CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight()) / 2;
+		
+		
+		
+		//ground
+		this.mPhysicsWorld = new FixedStepPhysicsWorld(30, new Vector2(0, SensorManager.GRAVITY_EARTH+9), false, 3, 2);
+
+		final Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2, this.getVertexBufferObjectManager());
+		final Rectangle roof = new Rectangle(0, 0, CAMERA_WIDTH, 2, this.getVertexBufferObjectManager());
+		final Rectangle left = new Rectangle(0, 0, 2, CAMERA_HEIGHT, this.getVertexBufferObjectManager());
+		final Rectangle right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT, this.getVertexBufferObjectManager());
+
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
+		
+		this.mScene.attachChild(ground);
+		
+
+		this.mScene.registerUpdateHandler(this.mPhysicsWorld);
+		
+
+		/* Calculate the coordinates for the face, so its centered on the camera. */
+		final float playerX = (CAMERA_WIDTH - this.mPlayerTextureRegion.getWidth()) / 2;
+		final float playerY = CAMERA_HEIGHT - this.mPlayerTextureRegion.getHeight() - 5;
+
+		/* Create two sprits and add it to the scene. */
+		final AnimatedSprite player = new AnimatedSprite(playerX-200, CAMERA_HEIGHT - 3, this.mPlayerTextureRegion, vertexBufferObjectManager);
+		player.setScaleCenterY(this.mPlayerTextureRegion.getHeight());
+		player.setScale(3.5f);
+		this.mBoundChaseCamera.setChaseEntity(player);
+				
+				
+		
+		
+		//Controls
+		this.mDigitalOnScreenControl = new DigitalOnScreenControl(0, CAMERA_HEIGHT - this.mOnScreenControlBaseTextureRegion.getHeight(), this.mCamera, this.mOnScreenControlBaseTextureRegion, this.mOnScreenControlKnobTextureRegion, 0.1f, this.getVertexBufferObjectManager(), new IOnScreenControlListener() {
+			public void onControlChange(final BaseOnScreenControl pBaseOnScreenControl, final float pValueX, final float pValueY) {
+				Vector2 velocity=new Vector2(pValueX*5, 0);
+								playerbody.setLinearVelocity(velocity);
+								
+
+				if(!player.isAnimationRunning() && !player.isAnimationRunning())
+	                if(pValueX>0 && !player.isAnimationRunning()){//Derecha
+	                    direct=true;
+	                	player.animate(new long[]{100, 100, 100}, 3, 5, false);
+	                }else{
+	                    if(pValueX<0){//Izquierda
+	                        direct=false;
+	                    	player.animate(new long[]{100, 100, 100}, 9, 11, false);
+	                    }
+	               
+	                }
+				
+			}
+			
+			
+			
+			
+		});
+		
+				
+		this.mDigitalOnScreenControl.getControlBase().setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);
+		this.mDigitalOnScreenControl.getControlBase().setAlpha(0.5f);
+		this.mDigitalOnScreenControl.getControlBase().setScaleCenter(0, 128);
+		this.mDigitalOnScreenControl.getControlBase().setScale(1.25f);
+		this.mDigitalOnScreenControl.getControlKnob().setScale(1.25f);
+		this.mDigitalOnScreenControl.refreshControlKnobPosition();
+		
+		
+		
+	//jump	
+		ButtonSprite button = new  ButtonSprite(CAMERA_WIDTH-120, CAMERA_HEIGHT-90, mButtonTextureRegion, vertexBufferObjectManager) {
+
+			public boolean onAreaTouched(TouchEvent pTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY) {
+		           if(pTouchEvent.isActionDown()) {
+		        	   if(player.getY() > 5){//ground.getY()-95){
+		        		   jump();
+		        	   }
+		        	   else{
+		        		   
+		        	   }
+		        			           }
+		           return super.onAreaTouched(pTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+		       }
+		};
+		button.setAlpha(0.5f);
+		button.setScale(1.5f);
+		
+
+		mScene.setChildScene(this.mDigitalOnScreenControl);
+		
+		mScene.attachChild(button);
+		mScene.registerTouchArea(button);
+		mScene.attachChild(player);
+		
+		
+		mScene.registerUpdateHandler(this.mPhysicsWorld);
+
+		return mScene;
+		
+		
+	}
+	
+	protected void jump() {
+		// TODO Auto-generated method stub
+		int x=0;
+		if(direct){
+			x=7;
+			
+		}
+		else{
+			x=-7;
+		}
+		
+		playerbody.setLinearVelocity(new Vector2(playerbody.getLinearVelocity().x,-15f));
+	    playerbody.setLinearVelocity(new Vector2(x,playerbody.getLinearVelocity().y));;
+  	  
+  	   
+	}
+
+	    
+}
